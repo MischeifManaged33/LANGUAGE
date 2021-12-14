9c169b622ec7b3974dfe2fece96994451eabe8cc
Emma Albright <42463223+MischeifManaged33@users.noreply.github.com>
2021-12-14 03:43:37 -0600

    gets playlist by link

--- Spotify/getMe.js
+++ Spotify/getMe.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const SpotifyWebApi = require('spotify-web-api-node');
 const token = "BQDnIKHIvzB4ko7asOjJKcP9zOWICLylLOBpWnMF1iNtM3Le5fVupSwwU-NH0mZMuSQvn8KjNAJaRb2tcZ80BjBc4n85qFku4Vwa3tHRaXgUMfK6Ikpm6WHiEdDYlz88aty981CnedwA2XOsLbYO9cAGUAMmA0NgNfL6MXcCDsC4uykDSSc7W3ctVyCAvuWiEEZxvz0PwYlt_ODfrEdMxsiL6N7rPupeETOZxkUPZa7XnnZCAXto_j_6iO6gR5jAqF4WVaEaZNztoN1zpDl7XrBMLcirJ8Yi7fhJq5c6702GBto7SjJq";
 
+
 const spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken(token);
 
@@ -12,4 +13,11 @@ async function getPlaylist(playlistID){
     }
 }
 
-getPlaylist('0ZRnqlBSbc9lprG8w7HvMK');
+function getThePlaylistID(){
+    var input = "https://open.spotify.com/playlist/0ZRnqlBSbc9lprG8w7HvMK";
+    var link = input.split("/");
+    input = link[link.length - 1];
+    return input;
+}
+
+getPlaylist(getThePlaylistID());
