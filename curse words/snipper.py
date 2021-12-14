fc2d08b263252ac06629ca0c7f0901ad69831103
Emma Albright <42463223+MischeifManaged33@users.noreply.github.com>
2021-12-14 01:20:07 -0600

    now makes words uppercase

--- curse words/snipper.py
+++ curse words/snipper.py
@@ -2,7 +2,7 @@ original = open('words.txt', "r")
 new = open('improved.txt', "w")
 
 for line in original:
-  line = line.split(" ")[0]
+  line = line.split(" ")[0].upper()
   new.write(line + "\n")
   print(line)
 
