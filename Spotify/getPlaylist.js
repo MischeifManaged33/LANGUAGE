507b40101a0b3626619949e74d174bd298b12520
Emma Albright <42463223+MischeifManaged33@users.noreply.github.com>
2021-12-14 02:09:39 -0600

    Spotify Folder

---
+++ Spotify/getPlaylist.js
@@ -0,0 +1,3 @@
+var client_id = '54d8295f37044b948ad28e50fe6f52e7'; // Your client id
+var client_secret = 'd953d79686664191a8221dbba0a20945'; // Your secret
+var redirect_uri = ' http://localhost:8888/callback'; // Your redirect uri
